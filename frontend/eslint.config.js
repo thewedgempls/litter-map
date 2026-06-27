a44fd8bc543f0b3bccf23d85b752c164444fb2a3
Preston Locke <me@prestonlocke.net>
2026-06-27 13:10:18 -0500

    Add HTML/CSS/JS lint pull request checks on frontend code

---
+++ frontend/eslint.config.js
@@ -0,0 +1,34 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  { ignores: ["docs/**", "node_modules/**"] },
+  {
+    files: ["litter-map.js"],
+    languageOptions: {
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+        L: "readonly",
+        firebase: "readonly",
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      // Top-level functions are called from inline HTML event handlers
+      "no-unused-vars": ["error", {
+        vars: "local",
+        args: "after-used",
+        ignoreRestSiblings: true,
+      }],
+    },
+  },
+  {
+    files: ["build.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: { ...globals.node },
+    },
+    rules: { ...js.configs.recommended.rules },
+  },
+];
